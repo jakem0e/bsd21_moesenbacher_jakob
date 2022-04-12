756cfb66210eb48461116c62475a90310bad70d8
Jakob Moesenbacher <52100744@edu.campus02.at>
2022-04-12 23:48:58 +0200

    Finalisierung der 3. Uebung

--- exercise3.md
+++ exercise3.md
@@ -1,1 +1,10 @@
 # Exercise 3
+
+
+##Ordner
+Es gibt nun auch die Klasse Main.class im target 0rdner
+![Picture 1](media/ex3.PNG)
+
+
+##Java ausgeführt
+![Java executed](media/ex3_1.PNG)
---
+++ src/main/java/at/campus02/bsd/Main.java
@@ -0,0 +1,12 @@
+package at.campus02.bsd;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(2, 5));
+        System.out.println(calculator.minus(2, 5));
+        System.out.println(calculator.divide(2, 5));
+        System.out.println(calculator.multiply(2, 5));
+    }
+
+}
