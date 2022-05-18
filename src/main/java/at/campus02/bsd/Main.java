55ab812a7514aaa86ec74d79527b93f5efac60ce
Jakob Moesenbacher <52100744@edu.campus02.at>
2022-05-18 11:14:12 +0200

    commit added Logging

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,7 +1,17 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    // Creating a logger
+    private static Logger logger = LogManager.getLogger();
+
+
     public static void main(String[] args) {
+
+        logger.info("Info Logger");
+        logger.error("Error Logger");
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(2, 5));
         System.out.println(calculator.minus(2, 5));
